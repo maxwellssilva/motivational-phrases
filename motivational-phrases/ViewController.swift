d04c48489cc8d12e96093caa703455ae1fb7880a
Maxwell Silva <maxwell.ssilvva@gmail.com>
2025-07-04 13:49:23 -0400

    Initial Commit

---
+++ motivational-phrases/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  motivational-phrases
+//
+//  Created by Maxwell Silva on 04/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
