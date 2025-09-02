34c4152f811fe85145d33c2684fb11c5f8028950
Maxwell Silva <maxwell.ssilvva@gmail.com>
2025-09-02 08:18:39 -0400

    update: add random element

--- motivational-phrases/ViewController.swift
+++ motivational-phrases/ViewController.swift
@@ -12,17 +12,17 @@ class ViewController: UIViewController {
     lazy var logoImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "logo")
-        image.contentMode = .scaleAspectFit
+        image.contentMode = .scaleAspectFill
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
     
     lazy var newPhraseButton: UIButton = {
         let button = UIButton()
-        button.setTitle("New phrase", for: .normal)
+        button.setTitle("Nova frase", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.setTitleColor(.lightGray, for: .highlighted)
-        button.backgroundColor = .systemGreen
+        button.backgroundColor = UIColor(named: "verde-escuro")
         button.layer.cornerRadius = 12
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(newPhraseButtonTapped), for: .touchUpInside)
@@ -43,14 +43,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupLayout()
-        fetchDataAPI()
+        randomPhrase()
     }
     
-    func fetchDataAPI() {
+    func randomPhrase() {
+        var frases: [String] = []
+        frases.append("Se estiver se sentindo desmotivado ou sentindo que não é bom o suficiente, incendeie seu coração.")
+        frases.append("Eu te amaria em todos os universos")
+        frases.append("A obsessão ganha do talento todas às vezes")
+        
+        phraseLabel.text = frases.randomElement()
     }
     
     @objc private func newPhraseButtonTapped() {
-        fetchDataAPI()
+        randomPhrase()
     }
     
     private func setupLayout() {
@@ -60,14 +66,18 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImage.heightAnchor.constraint(equalToConstant: 150),
+            logoImage.widthAnchor.constraint(equalToConstant: 150),
             
-            newPhraseButton.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 30),
+            phraseLabel.topAnchor.constraint(equalTo: logoImage.bottomAnchor),
+            phraseLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            phraseLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            phraseLabel.bottomAnchor.constraint(equalTo: newPhraseButton.topAnchor),
+            
+            newPhraseButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
             newPhraseButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 70),
             newPhraseButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -70),
             
-            phraseLabel.topAnchor.constraint(equalTo: newPhraseButton.bottomAnchor, constant: 50),
-            phraseLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            phraseLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
 }
